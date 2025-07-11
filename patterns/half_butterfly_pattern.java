57e31405f07b768b9188f5d85b12b97271543908
Karan Gaikwad <kg263585@gmail.com>
2025-07-11 19:14:41 +0530

    added file

--- patterns/half_butterfly_pattern.java
+++ patterns/half_butterfly_pattern.java
@@ -2,19 +2,24 @@ public class half_butterfly_pattern {
     public static void main(String[] args) {
         // for n rows 
         // take spaces = 2*(n-i)
-        int n=4;
+        int n=40;
+
+        // to print upper body
         for(int i=1;i<=n;i++)
         {
+            // to print left stars
             for(int j=1;j<=i;j++)
             {
                 System.out.print("* ");
             }
             //for spaces 
             // we can also take for spaces 8-(i+i)
+            // rows =4
             for(int k=1;k<=2*(n-i);k++)
             {
                 System.out.print("  ");
             }
+            // to print right stars
             for(int l=1;l<=i;l++)
             {
                 System.out.print("* ");
