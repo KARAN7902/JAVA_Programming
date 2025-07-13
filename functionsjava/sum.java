be36938dc35e29b23b4479acd8a276fdfec8a80c
Karan Gaikwad <kg263585@gmail.com>
2025-07-13 11:28:51 +0530

    updated

--- functionsjava/sum.java
+++ functionsjava/sum.java
@@ -3,8 +3,13 @@ package functionsjava;
 import java.util.Scanner;
 
 public class sum {
+
+    // function declaration
     public static int printSum(int a,int b)
     {
+        // this function takes two user inputs that are integer and 
+        // calculates their sum
+        // and return it to main function 
         int sum = a+b;
         return sum;
     }
@@ -13,6 +18,7 @@ public class sum {
         {
             int a = sc.nextInt();
             int b = sc.nextInt();
+            // function calling
             int sum =printSum(a,b);
             System.out.println("the sum is:"+sum);
         }
