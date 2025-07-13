30b9381913fc72fb4ce4947f9df7026dacdd9993
Karan Gaikwad <kg263585@gmail.com>
2025-07-13 15:07:10 +0530

    updated

--- patterns/h_butterfly.java
+++ patterns/h_butterfly.java
@@ -1,56 +1,49 @@
 public class h_butterfly {
     public static void main(String[] args) {
-        int n=4;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j =1;j<=i;j++)
-            {
-                if(j==1 || i==j)
-                {
+        int n = 4;
+        // upper body
+        for (int i = 1; i <= n; i++) {
+            // for left hollo triangle
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
                     System.out.print("*");
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
-            for(int k=1;k<=2*(n-i);k++)
-            {
+            // for spaces between two triangle
+            for (int k = 1; k <= 2 * (n - i); k++) {
                 System.out.print(" ");
             }
-            
-            for(int j =1;j<=i;j++)
-            {
-                if(j==1 || i==j)
-                {
+            // for right triangle
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
                     System.out.print("*");
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
 
             System.out.println();
         }
-        for(int i=n;i>=1;i--)
-        {
-            for(int j =1;j<=i;j++)
-            {
-                if(j==1 || i==j)
-                {
+
+        // lower body
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
                     System.out.print("*");
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
-            for(int k=1;k<=2*(n-i);k++)
-            {
+            for (int k = 1; k <= 2 * (n - i); k++) {
                 System.out.print(" ");
             }
-            
-            for(int j =1;j<=i;j++)
-            {
-                if(j==1 || i==j)
-                {
+
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
                     System.out.print("*");
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
