aed648b9e7497f3ebb81737657197c8d6d4518fb
Karan Gaikwad <kg263585@gmail.com>
2025-07-15 23:21:51 +0530

    added file

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -1,19 +1,63 @@
 package Strings;
-import java.util.*;
+
+// import java.util.*;
+
 public class Strings {
     public static void main(String[] args) {
-        // this data type can contain a singl word or " " blank spaces as character or full setence
-        // String name ="karan";     
+        // this data type can contain a singl word or " " blank spaces as character or
+        // full setence
+        // String name ="karan";
         // String fullName="karan Gaikwad";
         // String sentence ="hi i a am java student";
-    // user input string
-     try(Scanner sc =new Scanner(System.in);)
-      {
-        // to take a single word or full line withot spaces we use sc.next();
-        // to take input full line with spaces we use sc.nextline();
-        String _name = sc.nextLine();
+        // ** user input string
+        // try(Scanner sc =new Scanner(System.in);)
+        // {
+        // // to take a single word or full line withot spaces we use sc.next();
+        // // to take input full line with spaces we use sc.nextline();
+        // String _name = sc.nextLine();
+        // System.out.println("name :"+_name);
+        // }
+        // **String concatenation
+        // using + operator add the both strings
 
-        System.out.println("name :"+_name);
-      }
+        // ** string length
+        // String nme = "karan";
+        // System.out.println(nme.length());
+
+        // ** substring
+        // String name ="karan Gaikwad";
+        // // requires substring(start index,end index);
+        // String lastName =name.substring(6);
+        // System.out.println(lastName);
+
+        // acess characters one by one
+
+        // String name = "karan";
+        // for (int i = 0; i < name.length(); i++) {
+        //     System.out.println(name.charAt(i));
+        // }
+        // /* out put
+        //  * k
+        //  * a
+        //  * r
+        //  * a
+        //  * n
+        //  */
+        // String compare compareTo() function
+
+        // String name1="karan";
+        // String name2="karan1";
+        // if(name1.compareTo(name2)==0)
+        // {
+        //     System.out.println("string are equal");
+        // }else{
+        //     System.out.println("string are  not equal");
+        // }
+
+        // String name ="karan";
+        // for(int i=name.length()-1;i>=0;i--)
+        // {
+        //     System.out.print(name.charAt(i));
+        // }
     }
 }
