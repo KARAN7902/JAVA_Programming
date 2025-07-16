fdd669e3c2a21557d43c95cae00ab70975d20e29
Karan Gaikwad <kg263585@gmail.com>
2025-07-16 21:35:06 +0530

    added new pg

---
+++ Strings/inString.java
@@ -0,0 +1,18 @@
+public class inString {
+    public static void main(String[] args) {
+        StringBuilder sc= new StringBuilder("elephant");
+        String resuilt="";
+        for(int i=0;i<sc.length();i++)
+        {
+            if(sc.charAt(i)=='e')
+            {
+                resuilt+='i';
+            }
+            else{
+                resuilt+=sc.charAt(i);
+            }
+        }
+        System.out.println(resuilt);
+    }
+    
+}
