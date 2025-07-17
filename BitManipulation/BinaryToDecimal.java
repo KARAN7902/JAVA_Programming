57fd3c73abc2dda84181870b79046904e82d4d8d
Karan Gaikwad <kg263585@gmail.com>
2025-07-17 23:34:05 +0530

    updated

--- BitManipulation/BinaryToDecimal.java
+++ BitManipulation/BinaryToDecimal.java
@@ -1,11 +1,12 @@
 package BitManipulation;
-import java.util.*;
+// import java.util.*;
 public class BinaryToDecimal {
     public static String DecimaltoBinary(int n){
         return Integer.toBinaryString(n);
     }
     public static int BinarytoDecimal(String binary){
         return Integer.parseInt(binary, 2);
+        // radix can be 2 for binary  8 for octal 16 for hexal
     }
     public static void main(String[] args) {
         int n =25;
