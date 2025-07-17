750fee158c14eb8542ec9c5528ebe2456a859738
Karan Gaikwad <kg263585@gmail.com>
2025-07-17 21:17:35 +0530

    updated

--- BitManipulation/SetBit.java
+++ BitManipulation/SetBit.java
@@ -2,10 +2,11 @@ package BitManipulation;
 
 public class SetBit {
 public static void main(String[] args) {
-    int n=5;
-    int bitMask =1<<1;
+    int n=5;//0101
+    int bitMask =1<<1;//0010
 
    int newNumber = bitMask | n;
+   //0111
    System.out.println(newNumber);     
 }    
 }
