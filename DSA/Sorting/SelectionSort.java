d111b25795d07046444a2ad1517b8767ae9ad643
Karan Gaikwad <kg263585@gmail.com>
2025-07-18 00:53:51 +0530

    updated

---
+++ DSA/Sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+package Sorting.Sorting1;
+
+public class SelectionSort {
+    public static void PrintArray(int arr[]) {
+        
+        for (int i = 0; i < arr.length; i++) {
+            
+            System.out.print(arr[i]+"\t");
+        }
+        System.out.println();
+
+    }
+    public static void main(String[] args) {
+        int a[]={6,3,8,2,9};
+         for(int j=0;j<a.length-1;j++)
+         {
+            int smallest =j;
+            for(int i=j+1;i<a.length;i++)
+            {
+                if(a[smallest]>a[i])
+                {
+                    smallest =i;
+                }
+            }
+
+                    int temp =a[smallest];
+            a[smallest]=a[j];
+            a[j]=temp;
+
+         }
+     PrintArray(a);
+ 
+    }   
+}
