3320201f07433f89103ea14f6193b54ca5bce419
Karan Gaikwad <kg263585@gmail.com>
2025-07-18 23:55:29 +0530

    updated

--- OOPS/PolyMorphism/FunctionOverLoading.java
+++ OOPS/PolyMorphism/FunctionOverLoading.java
@@ -1,5 +1,31 @@
 package OOPS.PolyMorphism;
 
+class Students {
+    String name;
+    int age;
+
+    public void Display(String name) {
+        System.out.println(name);
+    }
+
+    public void Display(int age) {
+        System.out.println(age);
+    }
+
+    public void Display(String name, int age) {
+        System.out.println("name= "+ name +" age= "+ age);
+        // System.out.println(age);
+    }
+}
+
 public class FunctionOverLoading {
-    
+    public static void main(String[] args) {
+        Students s1 = new Students();
+        s1.name="kartik";
+        s1.age=23;
+        s1.Display(s1.name);
+        s1.Display(s1.age);
+        s1.Display(s1.name,s1.age);
+    }
+
 }
