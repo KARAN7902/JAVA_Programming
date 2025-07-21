333d766080796228f23fe45f6c87807f59a25d33
Karan Gaikwad <kg263585@gmail.com>
2025-07-21 18:35:28 +0530

    updated

--- Packeges/MyClass.java
+++ Packeges/MyClass.java
@@ -1,19 +1,23 @@
 package Packeges;
-//import Packeges.Accounts.*;;
+import Packeges.Accounts.*;;
 
 class Myclass{
   public int age;
   protected String name1;
-// this protected string allow to access only same class
+// this protected string allow to access only same class and the sub classes
 //
+ private String password;
+ // to print the password
   public String GetName(){
-    return this.name1;
+    return this.password;
   }
+  // to change the password
    public void SetName(String name){
-   this.name1=name;
+   this.password=name;
   }
+  // to print the passsword
   public void Print(){
-    System.out.println(this.name1);
+    System.out.println(this.password);
   }
 }
 class Karan extends Myclass{
@@ -30,9 +34,10 @@ public class MyClass {
       System.out.println(name1.name1);
       name1.SetName("kartik");
       name1.Print();
-    }
 
-    Karan kg=new Karan();     
-  
+      Karan kg =new Karan();
+      // the protected String is aacessed by the child class
+      kg.name1 ="kartik";
+    }  
 }
 
