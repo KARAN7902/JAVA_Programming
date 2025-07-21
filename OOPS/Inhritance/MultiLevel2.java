b5aab6777a68053faefd3a8cf41e18f2190ed83e
Karan Gaikwad <kg263585@gmail.com>
2025-07-22 00:37:36 +0530

    updated

--- OOPS/Inhritance/MultiLevel2.java
+++ OOPS/Inhritance/MultiLevel2.java
@@ -17,7 +17,13 @@ public class MultiLevel2 extends MultiLevel1{
     }
 
      public static void main(String[] args) {
-        
+        MultiLevel2 m2 =new MultiLevel2();
+        // this is the property of  multilevel class
+        m2.name="kartik";
+        // this property  is of  multilevel 1 class
+        m2.color="gora";
+        // this property is of multilevel 2 class
+        m2.Print();
      }
 
 }
