7fea4eed424ebed9716cb3b477c238df90190885
Karan Gaikwad <kg263585@gmail.com>
2025-07-23 20:36:05 +0530

    updated

---
+++ Hacker_rank/date.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.DayOfWeek;
+public class date {
+  
+   public static String findDay( String inputDate) {
+      LocalDate date = LocalDate.parse(inputDate,DateTimeFormatter.ofPattern("MM-dd-yy"));
+      DayOfWeek dayOfWeek =date.getDayOfWeek();
+      return dayOfWeek.toString();
+    }
+
+       public static void main(String[] args) throws IOException {
+       System.out.println(findDay("08 05 2015"));
+    }
+}
+
