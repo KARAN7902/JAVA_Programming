ab4e960d13607b1ace30cb91d34996f28f98bf62
Karan Gaikwad <kg263585@gmail.com>
2025-08-01 00:22:58 +0530

    small updated

--- BitManipulation/Countones.java
+++ BitManipulation/Countones.java
@@ -1,6 +1,23 @@
 package BitManipulation;
 
 public class Countones {
+
+     public static int Consecative(int n){
+        String str =Integer.toBinaryString(n);
+        int count =0;
+        int i=0;
+        while(i<str.length()){
+            if(str.charAt(i)=='1'){
+                count++;
+                i++;
+            }else{
+                count=0;
+                i++;
+            }
+        }
+      return count;
+     }
+
     public static int cout1s(int n){
         int count =0;
         while (n!=0) {
@@ -10,13 +27,14 @@ public class Countones {
         return count;
     }
     public static void main(String[] args) {
-        int n=951;
-       System.out.printf("no of one's in %d are %d\n",n,cout1s(n));
+        int n=5;
+    //    System.out.printf("no of one's in %d are %d\n",n,cout1s(n));
        // we can also use the built in function to  count no of one's in the number 
        //Integer.bitCount(n); 
-      int count1 =Integer.bitCount(n);
-     System.out.println(count1);
+    //   int count1 =Integer.bitCount(n);
+    //  System.out.println(count1);
      System.out.println(Integer.toBinaryString(n));
+     System.out.println(Consecative(n));
     }
     
 }
