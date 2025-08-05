a2ff38e591c67ce2b80fdc2ad81ec1ffdd4c437f
Karan Gaikwad <kg263585@gmail.com>
2025-08-06 00:26:56 +0530

    update

---
+++ OOPS/obj.java
@@ -0,0 +1,24 @@
+class obj1{
+ 
+   int x;
+   int y;
+   
+   void print(){
+    System.out.println(x);
+    System.out.println(y); 
+   }
+
+}
+
+
+class obj{
+  public static void main(String[] args) {
+  obj1 o1 =new obj1();
+  o1.x=5;
+  System.out.println(o1.x);    
+  System.out.println(o1.y); 
+  // here the inialsation od y in not defined or not specified so in java b default 
+  //it stores at the empty locaton '0' not any  grabage  value   
+
+  }
+}
