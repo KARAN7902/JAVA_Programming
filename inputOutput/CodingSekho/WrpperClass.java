df4983176fa4abc63682284e80a9cf83a6bc83eb
Karan Gaikwad <kg263585@gmail.com>
2025-08-08 00:52:33 +0530

    updated

---
+++ inputOutput/CodingSekho/WrpperClass.java
@@ -0,0 +1,36 @@
+package CodingSekho;
+// IMP
+public class WrpperClass {
+    // to make java 100% object oriented programming language
+    //e.g
+    // 1)Integer class
+    int x =Integer.parseInt(null);
+    // static method
+// return the integer value
+    Integer x1=Integer.valueOf(null);
+    // static method
+    // returns the object  type data of type integer 
+    // x1 is the refrence variable
+    // both the above functions are static 
+         int y =x1.intValue();
+         // this function also returns integer value
+         // and it is instance method 
+
+
+
+
+      // 2) Double class
+      
+      
+      double x3 =Double.parseDouble(null);
+      Double x5 =Double.valueOf(x);
+      double y1 =x5.doubleValue();
+
+      // 3) float class
+
+      float x4 =Float.parseFloat(null);
+      Float f1 =Float.valueOf(null);
+      float y2 =f1.floatValue(); 
+
+
+}
