022a161302bed2b85d17394aa9862998e09d107e
Karan Gaikwad <kg263585@gmail.com>
2025-09-04 00:50:24 +0530

    small updated

--- inputOutput/CodingSekho/StaticMembers.java
+++ inputOutput/CodingSekho/StaticMembers.java
@@ -24,7 +24,9 @@ public class StaticMembers {
         A a2=new A();
         a1.fun1();// y=22
         a2.fun3();// y=45
-        a1.fun2();// print y it prints 45 because the y is the static variable  and it is the part of the the oject of the class 
+        a1.fun2();// print y it prints 45 because the y is the static variable  and it is the part of the class not the object
+        // it not accesible by the object ,it is accessible by the class itself
+        //i.e. A.y  
         // if any update is done by a2 the a2 is able to see the changes or the updated value of static member
      }
 }
